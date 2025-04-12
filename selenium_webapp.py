5556d0869b8747722526c4a47e44578e4db23fe8
Rohan Singhal <rohan.singhal@gmail.com>
2025-04-12 18:09:24 -0400

    add driver_version

--- selenium_webapp.py
+++ selenium_webapp.py
@@ -11,7 +11,7 @@ def download_selenium():
 	chrome_options.add_argument("--headless")
 	chrome_options.add_argument("--no-sandbox")
 	chrome_options.add_argument("--disable-dev-shm-usage")
-	driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+	driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version='114.0.5735.90').install()), options=chrome_options)
 	driver.get("https://google.com")
 	title = driver.title
 	language = driver.find_element(By.XPATH, "//div[@id='SIvCob']").text
